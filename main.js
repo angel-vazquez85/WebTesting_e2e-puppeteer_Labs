6fbfd2144817e77142b63a9f07e98d60100ccce7
Alberto Basalo <albertobasalo@hotmail.com>
2020-06-23 11:53:07 +0200

    feat: 🆗 validity

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const { getBrowser, closeBrowser, takeScreenshot } = require(`./lib/puppets`);
+const testExistence = require(`./tests/existence`);
+const testContent = require(`./tests/content`);
+const testInteraction = require(`./tests/interaction`);
+const testEmulation = require(`./tests/emulation`);
+const testValidity = require(`./tests/validity`);
 async function test() {
+  const { browser, pagePuppet } = await arrangeBefore();
+  // await testExistence(pagePuppet);
+  // await testContent(pagePuppet);
+  // await testInteraction(pagePuppet);
+  // await testEmulation(pagePuppet);
+  // await testValidity( pagePuppet );
+  // await takeScreenshot( pagePuppet );
+  await cleanAfter(browser);
+}
+async function arrangeBefore() {
   const browser = await getBrowser();
   const pagePuppet = await browser.newPage();
-  // await require(`./tests/existence`)(pagePuppet);
-  // await require(`./tests/content`)(pagePuppet);
-  // await require(`./tests/interaction`)(pagePuppet);
-  // await require( `./tests/emulation` )( pagePuppet );
-  await require(`./tests/validity`)(pagePuppet);
-  await takeScreenshot(pagePuppet);
+  return { browser, pagePuppet };
+}
+async function cleanAfter(browser) {
   await closeBrowser(browser);
 }
+
 test();
--- tests/validity.js
+++ tests/validity.js
@@ -3,7 +3,7 @@ const { given, when, then } = require(`../lib/bit.tester`);
 module.exports = async function (pagePuppet) {
   await given(`A site url`, async () => {
     const inputPageUrl = `https://www.bitademy.com`;
-    await when(`we scrap it for broken links`, async () => {
+    await when(`we scrap it for empty links`, async () => {
       await pagePuppet.goto(inputPageUrl, { waitUntil: `load` });
       const actual = await pagePuppet.evaluate(() =>
         window.find(`a:is(:not([href]),[href=""],[href="#"])`)
