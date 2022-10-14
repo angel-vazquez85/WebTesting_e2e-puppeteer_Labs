23f77c2bcdc288fe9df2db8b1cff9ef6cc861a70
Alberto Basalo <albertobasalo@hotmail.com>
2022-10-14 09:43:19 +0200

    fix: api testing

--- tests/api.js
+++ tests/api.js
@@ -1,32 +1,38 @@
-import request from 'supertest';
-import { given, then, when } from '../lib/bit.tester.js';
+import request from "supertest";
+import { given, then, when } from "../lib/bit.tester.js";
 
 export default async function api() {
   await getHello();
   await postProject();
 }
+
+// ************************************************************************************************
+// ⚡️ Start the the api server before running the tests
+// npm run api
+// ************************************************************************************************
+
 async function getHello() {
-  const inputHostUrl = `https://api-base.herokuapp.com`;
+  const inputHostUrl = `http://localhost:3000/`;
   await given(`the API url ${inputHostUrl}`, async () => {
-    const inputEndPoint = `/api/pub/hello`;
+    const inputEndPoint = `projects`;
     await when(`we call the ${inputEndPoint} endPoint`, async () => {
       const response = await request(inputHostUrl).get(inputEndPoint);
-      const actual = response.body.message;
-      const expected = `Hola Mundo`;
-      then(`respond with an Hola Mundo message`, actual, expected);
+      const actual = Array.isArray(response.body);
+      const expected = true;
+      then(`respond with an array`, actual, expected);
     });
   });
 }
 
 async function postProject() {
-  const inputHostUrl = `https://api-base.herokuapp.com`;
+  const inputHostUrl = `http://localhost:3000/`;
   await given(`the API url ${inputHostUrl}`, async () => {
-    const inputEndPoint = `/api/pub/projects`;
+    const inputEndPoint = `projects`;
     await when(`we post to the ${inputEndPoint} endPoint`, async () => {
-      const inputProject = { name: 'start testing', dueDate: '2020-12-31' };
+      const inputProject = { id: "start_testing", name: "start testing", startDate: "2020-12-31" };
       const response = await request(inputHostUrl).post(inputEndPoint).send(inputProject);
       const actual = response.body.name;
-      const expected = 'start testing';
+      const expected = "start testing";
       then(`respond with the same object`, actual, expected);
       const expectedStatus = 201;
       then(`respond with status code 201`, response.status, expectedStatus);
